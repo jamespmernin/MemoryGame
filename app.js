01ff96835091f8e58669a994502bedbd17d683f0
James Mernin <jamespmernin@gmail.com>
2021-08-06 10:14:10 -0400

    duplicated images in array

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/cheeseburger.png'
         },
         {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
             name: 'fries',
             img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
         },
         {
             name: 'hotdog',
@@ -18,8 +30,20 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/ice-cream.png'
         },
         {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
             name: 'milkshake',
             img: 'images/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
         },
         {
             name: 'pizza',
