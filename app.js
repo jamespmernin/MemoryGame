126726c3840524fe156b704b97d9ffa07c4852d5
James Mernin <jamespmernin@gmail.com>
2021-08-06 10:12:34 -0400

    array of images created

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-let plague = 0;
-let help = 0;
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+    ]
+})
